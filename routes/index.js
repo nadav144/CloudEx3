f9ef56f812d954576f730ba6069ed6c017fb0957
nadav144 <nadav@rotenberg.co.il>
2016-06-14 12:23:41 +0300

    log

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+// create a file only file logger
+var log = require('simple-node-logger').createSimpleFileLogger('project.log');
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.all('/', function(req, res, next) {
+
+  var ip = req.headers['x-forwarded-for'] ||
+      req.connection.remoteAddress ||
+      req.socket.remoteAddress ||
+      req.connection.socket.remoteAddress;
+  var userAgent = req.header("User-Agent");
+
+  log.info(ip + ";" + userAgent + ";" + req.method);
   res.render('index', { title: 'Express' });
 });
 
