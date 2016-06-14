39ffe79e351caddd9c53aed4ebeb1ac082ecad87
odedex <karitvruda@hotmail.com>
2016-06-14 14:57:57 +0300

    added textfile support

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+
+var textfilename = "textfile.txt";
 
 // create a file only file logger
 var log = require('simple-node-logger').createSimpleFileLogger('project.log');
@@ -16,5 +19,98 @@ router.all('/', function(req, res, next) {
   log.info(ip + ";" + userAgent + ";" + req.method);
   res.render('index', { title: 'Express' });
 });
+//
+// router.all('/newfile', function (req, res, next) {
+//     fs.writeFile(textfilename, "Hey there!", function(err) {
+//         if(err) {
+//             return console.log(err);
+//         }
+//
+//         console.log("The file was saved!");
+//     });
+// });
+
+router.all ('/text', function (req, res, next) {
+    console.log("IN PUT");
+    if (req.method !== "PUT") {
+       next();
+    } else {
+        if (req.body.text) {
+            var toAppend = req.body.text;
+            fs.appendFile(textfilename, toAppend + "\n", function (err) {
+                if (err) {
+                    return console.log(err);
+                } else {
+                    logFileSize();
+                }
+
+            })
+        } else {
+            return console.log("no param 'text' found");
+        }
+        res.end();
+    }
+});
+
+router.all ('/text/:rows', function (req, res, next) {
+    console.log("IN GET");
+    if (req.method !== "GET") {
+        next();
+    } else {
+        if (req.params.rows) {
+            var rows = req.params.rows;
+            fs.readFile(textfilename, 'utf-8', function(err, data) {
+                if (err){
+                    return console.log(err);
+                }
+                var lines = data.trim().split('\n');
+                var lastLines = lines.slice(Math.max(lines.length - rows, 1));
+
+                res.write(lastLines.join("\n"));
+                res.end();
+            });
+        } else {
+            res.end();
+            return console.log("no param 'rows' found");
+        }
+    }
+});
+
+router.all ('/text/:rows', function (req, res, next) {
+    console.log("IN DELETE");
+    if (req.method !== "DELETE") {
+        next();
+    } else {
+        if (req.params.rows) {
+            var rows = req.params.rows;
+            fs.readFile(textfilename, 'utf-8', function(err, data) {
+                if (err){
+                    return console.log(err);
+                }
+                var lines = data.trim().split('\n');
+                lines.splice(-1, rows);
+
+                fs.writeFile(textfilename, lines.join("\n") + "\n", function(err) {
+                    if (err) {
+                        return console.log(err);
+                    } else {
+                        logFileSize();
+                    }
+                })
+            });
+        } else {
+            return console.log("no param 'rows' found");
+        }
+        res.end();
+    }
+});
+
+function logFileSize() {
+    var stats = fs.statSync(textfilename);
+    var fileSizeInBytes = stats["size"];
+    //Convert the file size to megabytes (optional)
+    var fileSizeInKilobytes = fileSizeInBytes / 1024.0
+    log.info("logsize:" + fileSizeInKilobytes.toString());
+}
 
 module.exports = router;
